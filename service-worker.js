28708721cdfd57cee2481aade763ca3c12cce268
nurizzatibintirosli <37897213+nurizzatibintirosli@users.noreply.github.com>
2025-07-21 18:47:36 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,12 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('v1').then(function(cache) {
-            return cache.addAll([
-                './index.html',
-                './inventory.html',
-                './styles.css',
-                './app.js',
-                './manifest.json',
-                './bootstrap.bundle.min.js',
-                './bootstrap.min.css',
-                './sql-wasm.js',
-                './sql-wasm.wasm'
-            ]);
-        })
-    );
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
